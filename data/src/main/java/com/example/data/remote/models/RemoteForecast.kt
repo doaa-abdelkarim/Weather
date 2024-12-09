ff91699cba2f91ac1d44a1fdb9c227b8d0caa738
Doaa abdelkareem <doaa.abdelkareem@inovaeg.com>
2024-12-10 01:25:31 +0200

    [ENHANCE] Clean code

--- data/src/main/java/com/example/data/remote/models/RemoteForecast.kt
+++ data/src/main/java/com/example/data/remote/models/RemoteForecast.kt
@@ -8,44 +8,14 @@ data class RemoteForecast(
     @field:SerializedName("city")
     val city: City? = null,
 
-    @field:SerializedName("cnt")
-    val cnt: Int? = null,
-
-    @field:SerializedName("cod")
-    val cod: String? = null,
-
-    @field:SerializedName("message")
-    val message: Int? = null,
-
     @field:SerializedName("list")
     val remoteWeatherList: List<RemoteWeather?>? = null
 )
 
 data class City(
 
-    @field:SerializedName("country")
-    val country: String? = null,
-
-    @field:SerializedName("coord")
-    val coord: Coord? = null,
-
-    @field:SerializedName("sunrise")
-    val sunrise: Int? = null,
-
-    @field:SerializedName("timezone")
-    val timezone: Int? = null,
-
-    @field:SerializedName("sunset")
-    val sunset: Int? = null,
-
     @field:SerializedName("name")
-    val name: String? = null,
-
-    @field:SerializedName("id")
-    val id: Int? = null,
-
-    @field:SerializedName("population")
-    val population: Int? = null
+    val name: String? = null
 )
 
 fun RemoteForecast.asDomainModel(): List<Weather> =
