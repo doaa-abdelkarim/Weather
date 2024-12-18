8eb66f57efdd0bed010ff1f43a9b6de19d339791
Doaa abdelkareem <doaa.abdelkareem@inovaeg.com>
2024-12-18 21:02:23 +0200

    [ADD] build-logic module - part3

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -39,11 +39,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(project(":core"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    implementation(project(":core"))
     implementation(project(":domain"))
 
     // Dagger Hilt
