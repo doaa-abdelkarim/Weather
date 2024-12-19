9da4c6d9f9a0114c096e3ca7e39b5e44eeb7d44b
Doaa abdelkareem <doaa.abdelkareem@inovaeg.com>
2024-12-19 09:20:42 +0200

    [ADD] build-logic module - part4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlin.serialization)
-    id("kotlin-kapt")
-    id("kotlin-parcelize")
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.hilt)
 }
 
